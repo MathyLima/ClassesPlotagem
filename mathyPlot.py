f2c23d10d42bb24ddb03c0676368e04fbd31ca7d
MathyLima <mathy.l.freitas@gmail.com>
2023-05-30 19:09:14 -0300

    consegue plotar ate 3 classes pre-definidas

--- mathyPlot.py
+++ mathyPlot.py
@@ -63,41 +63,61 @@ class Plota():
     def classes(self,classe):
         for classes in classe:    
             self._classes.append(classes)
+            
     
     @property
-    def get_X(self):
-        return np.array(list((x for x in Classes(self.classes).get_x_Class )))    
+    def get_X(self):        
+        return np.array(list((ponto[1])for classes in self.classes for ponto in classes))   
     @property
     def get_Y(self):
-        return np.array(list((y for y in Classes(self.classes).get_y_Class )))    
+        return np.array(list((ponto[2])for classes in self.classes for ponto in classes))
         
+  
     def plota(self):
+        terceira_classe=False
         
         #  essa funcao deve percorrer o array de classes, que receberar todas as classes, que terao armazenados os pontos, a partir disso os pontos sera distribuidos e plotados
         #  sendo assim deve haver uma junção de graficos, cada um contendo os pontos de cada classe
+        classe_1=self.classes[0]
+        print(classe_1)
         
-        pegaPontosClasse= {'x': self.get_X,
-                           'y': self.get_Y
+        print(self.get_X)
+        pegaPontosClasse_1= {'x': np.array(list((ponto[1])for ponto in classe_1)),
+                             'y': np.array(list((ponto[2])for ponto in classe_1))
                              }   
         
-        #print(pegaPontosClasse['x'])
+        data_primeira_classe={'x': pegaPontosClasse_1['x'],
+                            'y': pegaPontosClasse_1['y']}
+        
+        classe_2=self.classes[1]
+        pegaPontos_Classe_2={'x':np.array(list((ponto[1])for ponto in classe_2)),
+                             'y':np.array(list((ponto[2])for ponto in classe_2))}
+        data_segunda_classe={'x':pegaPontos_Classe_2['x'],
+                            'y':pegaPontos_Classe_2['y']}
+        print(pegaPontosClasse_1['x'])
         
         # pegaClasse_2 = np.array((classe[1]) for classe in self.classes)
         # pegaPontosClasse_2 = {'x': np.array((coordenadas[1]) for coordenadas in self.classes),
         #                       'y': np.array((coordenadas[2]) for coordenadas in self.classes)}
         
-        # if len(self.classes) == 3:
-        #     pegaClasse_3 = np.array((classe[2]) for classe in self.classes)
-        #     pegaPontosClasse_3 = {'x': np.array((coordenadas[1]) for coordenadas in self.classes),
-        #                       'y': np.array((coordenadas[2]) for coordenadas in self.classes)}
+        if len(self.classes) >2:
+             terceira_classe=True
+             classe_3 = self.classes[2]
+             pegaPontos_Classe_3 = {'x': np.array(list((ponto[1])for ponto in classe_3)),
+                                    'y': np.array(list((ponto[2]) for ponto in classe_3))}
+             data_terceira_classe = {
+                                    'x':pegaPontos_Classe_3['x'],
+                                    'y':pegaPontos_Classe_3['y']}
             
         
         
-        data_primeira_classe={'x': pegaPontosClasse['x'],
-                            'y': pegaPontosClasse['y']}
-        print(data_primeira_classe['x'])
+        
+        #print(data_primeira_classe['x'])
         fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-        ax.scatter('x', 'y',data=data_primeira_classe)
+        ax.scatter('x','y',color='g',data=data_primeira_classe)
+        ax.scatter('x','y',color='r',data=data_segunda_classe)
+        if terceira_classe==True:
+            ax.scatter('x','y',color='b',data=data_terceira_classe)
         ax.set_xlabel('Posicao X')
         ax.set_ylabel('Posicao Y')
     
--- mathyPlot_teste.py
+++ mathyPlot_teste.py
@@ -8,13 +8,18 @@ quarta_coord = mtp.Coordenadas(40,80)
 quinta_coord = mtp.Coordenadas(100,40)
 sexta_coord = mtp.Coordenadas(80,50)
 
+setima_coord = mtp.Coordenadas(200,90)
+oitava_coord = mtp.Coordenadas(74,29)
+nona_coord = mtp.Coordenadas(150,4)
 primeira_classe=mtp.Classes()
 
 primeira_classe.coordenadas= primeira_coord.coordenada_ponto,segunda_coord.coordenada_ponto,terceira_coord.coordenada_ponto
 #print(primeira_classe.coordenadas)
-# segunda_classe = mtp.Classes()
-# segunda_classe.coordenadas= quarta_coord.coordenada_ponto,quinta_coord.coordenada_ponto,sexta_coord.coordenada_ponto
+segunda_classe = mtp.Classes()
+segunda_classe.coordenadas= quarta_coord.coordenada_ponto,quinta_coord.coordenada_ponto,sexta_coord.coordenada_ponto
 
+terceira_classe = mtp.Classes()
+terceira_classe.coordenadas = setima_coord.coordenada_ponto,oitava_coord.coordenada_ponto,nona_coord.coordenada_ponto
 #print(primeira_classe.get_x)
 #print(segunda_classe.get_x)
 # print(primeira_coord.coordenada_ponto)
@@ -23,7 +28,7 @@ primeira_classe.coordenadas= primeira_coord.coordenada_ponto,segunda_coord.coord
 
 teste = mtp.Plota()
 
-teste.classes= primeira_classe.coordenadas
+teste.classes= primeira_classe.coordenadas,segunda_classe.coordenadas,terceira_classe.coordenadas
 #print(type(teste.get_X))
 
 print(teste.classes)
