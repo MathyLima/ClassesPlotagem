be2a5c25def53ea1937ba890eec13970a11327bb
MathyLima <mathy.l.freitas@gmail.com>
2023-06-17 22:03:04 -0300

    Otimização Codigo

--- mathyPlot_teste.py
+++ mathyPlot_teste.py
@@ -1,8 +1,8 @@
-import mathyPlot1_1 as mtp
+import mathyPlot as mtp
 import numpy as np
 
-primeiro_rotulo = mtp.Rotulos(60,30,36,144,500)
-segundo_rotulo = mtp.Rotulos(20,40,16,16,500)
+primeiro_rotulo = mtp.Rotulos(60,30,36,144,1000)
+segundo_rotulo = mtp.Rotulos(20,40,16,16,10000)
 terceiro_rotulo = mtp.Rotulos(25,10,100,25,5)
 
 conjunto_rotulos = mtp.Conjunto_rotulos(primeiro_rotulo,segundo_rotulo,terceiro_rotulo)
