05a9782e74319c830bba72d1636f097701a101f8
MathyLima <mathy.l.freitas@gmail.com>
2023-09-05 15:29:39 -0300

    Interface Funcionando

--- mathyPlot_teste.py
+++ mathyPlot_teste.py
@@ -1,13 +1,16 @@
 import mathyPlot as mtp
-import numpy as np
+import tkinter as tk
 
-primeiro_rotulo = mtp.Rotulos(60,30,36,144,500000)
-segundo_rotulo = mtp.Rotulos(20,40,16,16,500000)
-terceiro_rotulo = mtp.Rotulos(25,10,100,25,500000)
 
-conjunto_rotulos = mtp.Conjunto_rotulos(primeiro_rotulo,segundo_rotulo,terceiro_rotulo)
+primeiro_rotulo = mtp.Rotulos(60, 30, 36, 144, 5000, 90, 16)
+segundo_rotulo = mtp.Rotulos(20, 40, 16, 16, 5000, 60, 16)
+terceiro_rotulo = mtp.Rotulos(25, 10, 100, 25, 5000, 5, 9)
 
-conjunto_rotulos.plotagem([10,40,60],[60,100,30])
+root = tk.Tk()
+
+conjunto_rotulos = mtp.Conjunto_rotulos(root, primeiro_rotulo, segundo_rotulo, terceiro_rotulo)
+
+root.mainloop()
 
 
 
